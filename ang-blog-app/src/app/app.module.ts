4ca1b887bdcf20b31e1dbcbc3f57fd71572495b4
Filipe <83041450+filipe1238@users.noreply.github.com>
2023-05-26 17:46:44 -0300

    added more components

--- ang-blog-app/src/app/app.module.ts
+++ ang-blog-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
     ContactUsComponent,
     SubscriptionFormComponent,
     CommentListComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
